5e2278cf242ff190845aa623772314424a1851ce
Vlad Khar <vladimir.iv.kharitonov@gmail.com>
2020-05-04 13:04:19 +0300

    init

---
+++ f4ntec/Controllers/TheatreController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using f4ntec.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace f4ntec.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TheatreController : ControllerBase
+    {
+
+        private readonly ILogger<TheatreController> _logger;
+
+        public TheatreController(ILogger<TheatreController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("~/GetPerformances")]
+        public IEnumerable<Spectacle> GetPerformances()
+        {
+            DateTime curentDate = DateTime.Now;
+            return Enumerable.Range(1, 5).Select(index => new Spectacle
+            {
+                Id = index,
+                Name = "name"+index,
+                AvailableTickets = index*10,
+                StartDataTime = curentDate.AddHours(index),
+                EndDataTime = curentDate.AddHours(index*1.5)
+            })
+            .ToArray();
+        }
+
+        [HttpPost]
+        public void CreateSpectacle([FromForm]SpectacleCreateDto spectacle)
+        {
+
+        }
+
+    }
+}
---
+++ f4ntec/Model/Poster.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace f4ntec.Model
+{
+    public class Spectacle
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int AvailableTickets { get; set; }
+        public DateTime StartDataTime { get; set; }
+        public DateTime EndDataTime { get; set; }
+    }
+    public class SpectacleCreateDto
+    {
+        public string Name { get; set; }
+        public int AvailableTickets { get; set; }
+        public DateTime StartDataTime { get; set; }
+        public DateTime EndDataTime { get; set; }
+    }
+
+    public class TicketBalance
+    {
+        public Spectacle Spectacle { get; set; }
+        public int Balance { get; set; }
+    }
+
+    public class TicketUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class BookingTicket
+    {
+        public int SpectacleId { get; set; }
+        public int TicketUserId { get; set; }
+        public int ReservedTickets { get; set; }
+    }
+}
